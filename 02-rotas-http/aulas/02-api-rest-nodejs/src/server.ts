768841cbbdebe6d145900459f35358a4f22ba71b
Nathan Santos <nathann.santoss2@gmail.com>
2024-07-14 23:29:01 -0300

    nodejs http and routes, module 1 and 2 - structure and database

---
+++ 02-rotas-http/aulas/02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+// GET, POST, PUT, PATCH, DELETE
+
+// http://localhost:3333/hello
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
